d5d966e48dfe4837fdad7ef7a468a3acab9f86cb
Dee-Abby <habeebulaholayinka@gmail.com>
2025-02-03 15:57:17 +0100

    new commit

--- task1/settings.py
+++ task1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1ahc^mqe))#z9s&qq(*_3ts4xko829agy3d##*i%*^c66!qy@9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
